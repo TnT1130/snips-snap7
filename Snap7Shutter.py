86d28b9c9297d487db5892df317a7792e1f0bb5d
melner <melner@mellop.local>
2019-11-24 19:20:48 +0100

    #3 + getStatus

---
+++ Snap7Shutter.py
@@ -0,0 +1,43 @@
+from Snap7Connection import Snap7Connection as s7con
+from utilitys import lg
+
+class Snap7Shutter():
+  def __init__(self, config):
+    self.__client = s7con(config.get("global"))
+    self.__rooms = config.get("secret")["rooms"].lower().split(",")
+    self.__lightPos = "alle,fenster,tuer".lower().split(",")
+    self.__readDB = int(config.get("global")["shutterread"])
+    self.__writeDB = int(config.get("global")["shutterwrite"])
+    self.__readOffset = int(config.get("global")["ShutterReadOffset".lower()]) * 8   
+    self.__writeOffset = int(config.get("global")["ShutterWriteOffset".lower()]) * 8 
+    if    self.__readOffset < len(self.__lightPos) * 8 * 2 \
+       or self.__writeOffset < len(self.__lightPos) * 8 * 4:
+       pass #todo 
+       #raise ValueError("Offset must be bigger than number lightpositions")
+
+  def __getReadOffset(self, room, pos):
+    offset = self.__rooms.index(room.lower()) * self.__readOffset
+    offset += self.__lightPos.index(pos.lower()) #tofo * 8 * 2
+    lg.debug("room: {}, type: {}, ReadOffset: {}".format(room, pos, offset))
+    return offset
+
+  def __getWriteOffset(self, room, pos):
+    offset = self.__rooms.index(room.lower()) * self.__writeOffset
+    offset += self.__lightPos.index(pos.lower()) #todo* 8 * 4
+    lg.debug("room: {}, type: {}, WriteOffset: {}".format(room, pos, offset))
+    return offset
+
+  def getStatus(self, room, pos):
+    #todo
+    #tmp = self.__client.readBit(self.__readDB, self.__getReadOffset(room, pos))
+    tmp = 1
+    lg.info("room: {}, type: {}, Status: {}".format(room, pos, tmp))
+    return tmp
+
+  def close(self, room, pos):
+    lg.info("room: {}, type: {}, Action: Close".format(room, pos))
+    self.__client.setBit(self.__writeDB, self.__getWriteOffset(room, pos) + 1)
+
+  def open(self, room, pos):
+    lg.info("room: {}, type: {}, Action: Open".format(room, pos))
+    self.__client.setBit(self.__writeDB, self.__getWriteOffset(room, pos))
--- Snips-Snap7.py
+++ Snips-Snap7.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from Snap7Light import Snap7Light
+from Snap7Shutter import Snap7Shutter
 from assistant import Assistant
 from utilitys import catchErrors, getSlotValue, lg, configureLogger
 #hermes: 'publish_continue_session', 'publish_end_session', 'publish_start_session_action', 'publish_start_session_notification',
@@ -10,6 +11,7 @@ from utilitys import catchErrors, getSlotValue, lg, configureLogger
 
 assist = Assistant()
 lights = Snap7Light(assist.get_config())
+shutter = Snap7Shutter(assist.get_config())
 
 
 # ======================================================================================================================
@@ -17,25 +19,56 @@ lights = Snap7Light(assist.get_config())
 def activateObject(hermes, intent_message): 
   location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
   device = getSlotValue(intent_message.slots, "device", "")
-  lightPlace = getSlotValue(intent_message.slots, "lightType", "decke")
-  lg.debug("activateObject:    location: {}, device: {}, deviceType: {}".format(location, device, lightPlace))
+  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
+  lg.debug("activateObject:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
   if device.upper() == "Licht".upper():
-    lights.turnOn(location, lightPlace)
+    deviceType = deviceType if deviceType != "default" else "decke"
+    lights.turnOn(location, deviceType)
     hermes.publish_end_session(intent_message.session_id, "Licht im {} wird angeschalten".format(location))
+  elif device.upper() == "Rollo".upper():
+    deviceType = deviceType if deviceType != "default" else "fenster"
+    shutter.open(location, deviceType)
+    hermes.publish_end_session(intent_message.session_id, "Rolladen im {} wird geöffnet".format(location))
   else:
-    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstüzt".format(device))
+    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
 
 @catchErrors
 def deactivateObject(hermes, intent_message):
   location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
   device = getSlotValue(intent_message.slots, "device", "")
-  lightPlace = getSlotValue(intent_message.slots, "lightType", "decke")
-  lg.debug("deactivateObject:    location: {}, device: {}, deviceType: {}".format(location, device, lightPlace))
+  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
+  lg.debug("deactivateObject:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
   if device.upper() == "Licht".upper():
-    lights.turnOff(location, lightPlace)
+    deviceType = deviceType if deviceType != "default" else "decke"
+    lights.turnOff(location, deviceType)
     hermes.publish_end_session(intent_message.session_id, "Licht im {} wird ausgeschalten".format(location))
+  elif device.upper() == "Rollo".upper():
+    deviceType = deviceType if deviceType != "default" else "fenster"
+    shutter.close(location, deviceType)
+    hermes.publish_end_session(intent_message.session_id, "Rolladen im {} wird geschlossen.".format(location))
   else:
-    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstüzt".format(device))
+    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
+
+@catchErrors
+def getObjectStatus(hermes, intent_message):
+  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+  device = getSlotValue(intent_message.slots, "device", "")
+  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
+  lg.debug("getObjectStatus:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
+  if device.upper() == "Licht".upper():
+    deviceType = deviceType if deviceType != "default" else "decke"
+    if lights.getStatus(location, deviceType) == 1:
+      hermes.publish_end_session(intent_message.session_id, "Das Licht im {} ist angeschalten".format(location))
+    else:  
+      hermes.publish_end_session(intent_message.session_id, "Das Licht im {} ist ausgeschalten".format(location))
+  elif device.upper() == "Rollo".upper():
+    deviceType = deviceType if deviceType != "default" else "fenster"
+    if shutter.getStatus(location, deviceType) == 1:
+      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist geöffnet.".format(location))
+    else:
+      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist geschlossen.".format(location))
+  else:
+    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
 
 
 
@@ -43,6 +76,7 @@ if __name__ == "__main__":
       configureLogger()
       h = assist.get_hermes()
       h.connect()
-      h.subscribe_intent("mkloeckner:ActivateObject", activateObject)
-      h.subscribe_intent("mkloeckner:DeactivateObject", deactivateObject)
+      h.subscribe_intent("abcderff:ActivateObject", activateObject)
+      h.subscribe_intent("abcderff:DeactivateObject", deactivateObject)
+      h.subscribe_intent("abcderff:GetObjectStatus", getObjectStatus)
       h.loop_forever()
