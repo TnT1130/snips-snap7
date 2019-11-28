7dafa19011f5cac4ef23c4bd887f4a87205a1d3c
userdomi <abcdeff@t-online.de>
2019-11-28 08:40:49 +0000

    bugfixes for #1 and #4

--- Snap7Connection.py
+++ Snap7Connection.py
@@ -40,8 +40,9 @@ class Snap7Connection():
       lg.debug("Write DB: {}, Byte: {}, Data: {}".format(db, offset, bin(tmpData)))
       self.__client.db_write(db, offset, bytearray([tmpData]))
 
-    def readInt(self, db, offset):
+    def readInt(self, db, index):
       self.connect()
+      offset = index // 8
       tmp = self.__client.db_read(db, offset, 2)
       tmpInt = int.from_bytes(tmp, byteorder='big', signed=True)
       lg.debug("readInt DB: {}, offset: {}, BinData: {}, Int: {}".format(db, offset, "b'" + (bin(tmp[0])[2:]).rjust(8, '0') \
@@ -59,15 +60,15 @@ class Snap7Connection():
     def connect(self):  
       if not self.__client:
         lg.info("connect to SPS {} password IP: {}, PORT: {}, RACK: {}, SLOT: {}".format(
+          "with" if self.__assistantConfig.get("secret").get("Snap7Password".lower(), "") != "" else "without",
           self.__assistantConfig.get("global")["spsip"],
           self.__assistantConfig.get("global").get("spsport", 102),
           self.__assistantConfig.get("global").get("spsrack", 0),
-          self.__assistantConfig.get("global").get("spsslot", 2),
-          "with" if self.__assistantConfig.get("secret").get("spsslot", "") != "" else "without"
+          self.__assistantConfig.get("global").get("spsslot", 2)
         ))
         self.__client = snap7.client.Client()
-        if self.__assistantConfig.get("secret").get("spsslot", "") != "":
-          self.__client.set_session_password(self.__assistantConfig.get("secret").get("spsslot", ""))
+        if self.__assistantConfig.get("secret").get("Snap7Password".lower(), "") != "":
+          self.__client.set_session_password(self.__assistantConfig.get("secret").get("Snap7Password".lower(), ""))
         self.__client.set_connection_type(2)
         self.__client.connect(self.__assistantConfig.get("global")["spsip"], 
                               int(self.__assistantConfig.get("global").get("spsrack", 0)), 
