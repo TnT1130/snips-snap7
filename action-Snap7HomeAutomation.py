0494a15b581f2d1a6032479a553529cd67170800
TnT1130 <58545433+TnT1130@users.noreply.github.com>
2019-12-07 22:38:47 +0100

    Update action-Snap7HomeAutomation.py

--- action-Snap7HomeAutomation.py
+++ action-Snap7HomeAutomation.py
@@ -17,87 +17,81 @@ temp = Snap7Temperature(assist.get_config())
 
 
 # ======================================================================================================================
-@catchErrors
-def activateObject(hermes, intent_message): 
-  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
-  device = getSlotValue(intent_message.slots, "device", "")
-  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
-  lg.debug("activateObject:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
-  if device.upper() == "Licht".upper():
-    deviceType = deviceType if deviceType != "default" else "decke"
-    lights.turnOn(location, deviceType)
-    hermes.publish_end_session(intent_message.session_id, "Licht im {} wird angeschalten".format(location))
-  elif device.upper() == "Rollo".upper():
-    deviceType = deviceType if deviceType != "default" else "fenster"
-    shutter.open(location, deviceType)
-    hermes.publish_end_session(intent_message.session_id, "Rolladen im {} wird geöffnet".format(location))
-  else:
-    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
+#@catchErrors
+#def activateObject(hermes, intent_message): 
+#  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+#  device = getSlotValue(intent_message.slots, "device", "")
+#  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
+#  lg.debug("activateObject:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
+#  if device.upper() == "Licht".upper():
+#    deviceType = deviceType if deviceType != "default" else "decke"
+#    lights.turnOn(location, deviceType)
+#    hermes.publish_end_session(intent_message.session_id, "Licht im {} wird angeschalten".format(location))
+#  elif device.upper() == "Rollo".upper():
+#    deviceType = deviceType if deviceType != "default" else "fenster"
+#    shutter.open(location, deviceType)
+#    hermes.publish_end_session(intent_message.session_id, "Rolladen im {} wird geöffnet".format(location))
+#  else:
+#    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
 
-@catchErrors
-def deactivateObject(hermes, intent_message):
-  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
-  device = getSlotValue(intent_message.slots, "device", "")
-  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
-  lg.debug("deactivateObject:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
-  if device.upper() == "Licht".upper():
-    deviceType = deviceType if deviceType != "default" else "decke"
-    lights.turnOff(location, deviceType)
-    hermes.publish_end_session(intent_message.session_id, "Licht im {} wird ausgeschalten".format(location))
-  elif device.upper() == "Rollo".upper():
-    deviceType = deviceType if deviceType != "default" else "fenster"
-    shutter.close(location, deviceType)
-    hermes.publish_end_session(intent_message.session_id, "Rolladen im {} wird geschlossen.".format(location))
-  else:
-    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
+#@catchErrors
+#def deactivateObject(hermes, intent_message):
+#  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+#  device = getSlotValue(intent_message.slots, "device", "")
+#  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
+#  lg.debug("deactivateObject:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
+#  if device.upper() == "Licht".upper():
+#    deviceType = deviceType if deviceType != "default" else "decke"
+#    lights.turnOff(location, deviceType)
+#    hermes.publish_end_session(intent_message.session_id, "Licht im {} wird ausgeschalten".format(location))
+#  elif device.upper() == "Rollo".upper():
+#    deviceType = deviceType if deviceType != "default" else "fenster"
+#    shutter.close(location, deviceType)
+#    hermes.publish_end_session(intent_message.session_id, "Rolladen im {} wird geschlossen.".format(location))
+#  else:
+#    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
 
-@catchErrors
-def getObjectStatus(hermes, intent_message):
-  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
-  device = getSlotValue(intent_message.slots, "device", "")
-  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
-  lg.debug("getObjectStatus:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
-  if device.upper() == "Licht".upper():
-    deviceType = deviceType if deviceType != "default" else "decke"
-    if lights.getStatus(location, deviceType) == 1:
-      hermes.publish_end_session(intent_message.session_id, "Das Licht im {} ist angeschalten".format(location))
-    else:  
-      hermes.publish_end_session(intent_message.session_id, "Das Licht im {} ist ausgeschalten".format(location))
-  elif device.upper() == "Rollo".upper():
-    deviceType = deviceType if deviceType != "default" else "fenster"
-    tmp = shutter.getStatus(location, deviceType)
-    if tmp == 0:
-      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist komplett geöffnet.".format(location))
-    elif tmp < 33:
-      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist leicht geschlossen.".format(location))
-    elif tmp < 66:
-      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist zur hälfte geschlossen.".format(location))
-    elif tmp < 100:
-      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist weit geschlossen.".format(location))
-    elif tmp == 100:
-      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist komplett geschlossen.".format(location))
-  else:
-    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
+#@catchErrors
+#def getObjectStatus(hermes, intent_message):
+#  location = getSlotValue(intent_message.slots, "deviceLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+#  device = getSlotValue(intent_message.slots, "device", "")
+#  deviceType = getSlotValue(intent_message.slots, "deviceType", "default")
+#  lg.debug("getObjectStatus:    location: {}, device: {}, deviceType: {}".format(location, device, deviceType))
+#  if device.upper() == "Licht".upper():
+#    deviceType = deviceType if deviceType != "default" else "decke"
+#    if lights.getStatus(location, deviceType) == 1:
+#      hermes.publish_end_session(intent_message.session_id, "Das Licht im {} ist angeschalten".format(location))
+#    else:  
+#      hermes.publish_end_session(intent_message.session_id, "Das Licht im {} ist ausgeschalten".format(location))
+#  elif device.upper() == "Rollo".upper():
+#    deviceType = deviceType if deviceType != "default" else "fenster"
+#    tmp = shutter.getStatus(location, deviceType)
+#    if tmp == 0:
+#      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist komplett geöffnet.".format(location))
+#    elif tmp < 33:
+#      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist leicht geschlossen.".format(location))
+#    elif tmp < 66:
+#      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist zur hälfte geschlossen.".format(location))
+#    elif tmp < 100:
+#      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist weit geschlossen.".format(location))
+#    elif tmp == 100:
+#      hermes.publish_end_session(intent_message.session_id, "Rolladen im {} ist komplett geschlossen.".format(location))
+#  else:
+#    hermes.publish_end_session(intent_message.session_id, "Gerätetyp {} wird aktuell nicht unterstützt".format(device))
 
-@catchErrors
-def getTemperature(hermes, intent_message):
-  ObjectLocation = getSlotValue(intent_message.slots, "ObjectLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
-  tempType = getSlotValue(intent_message.slots, "tempType", "Ist")
-  hermes.publish_end_session(intent_message.session_id, "Die Temperatur im {} beträgt {} Grad.".format(ObjectLocation, temp.getStatus(ObjectLocation, tempType) / 100.0))
+#@catchErrors
+#def getIncrease(hermes, intent_message):
+#  ObjectLocation = getSlotValue(intent_message.slots, "ObjectLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+#  tempChangeType = "Plus_" + getSlotValue(intent_message.slots, "tempChangeType", "1_00")
+#  temp.changeTemp(ObjectLocation, tempChangeType)
+#  hermes.publish_end_session(intent_message.session_id, "Temperatur im {} wird verändert.".format(ObjectLocation))
 
-@catchErrors
-def getIncrease(hermes, intent_message):
-  ObjectLocation = getSlotValue(intent_message.slots, "ObjectLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
-  tempChangeType = "Plus_" + getSlotValue(intent_message.slots, "tempChangeType", "1_00")
-  temp.changeTemp(ObjectLocation, tempChangeType)
-  hermes.publish_end_session(intent_message.session_id, "Temperatur im {} wird verändert.".format(ObjectLocation))
-
-@catchErrors
-def getDecrease(hermes, intent_message):
-  ObjectLocation = getSlotValue(intent_message.slots, "ObjectLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
-  tempChangeType = "Minus_" + getSlotValue(intent_message.slots, "tempChangeType", "1_00")
-  temp.changeTemp(ObjectLocation, tempChangeType)
-  hermes.publish_end_session(intent_message.session_id, "Temeperatur im {} wird verändert.".format(ObjectLocation))
+#@catchErrors
+#def getDecrease(hermes, intent_message):
+#  ObjectLocation = getSlotValue(intent_message.slots, "ObjectLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+#  tempChangeType = "Minus_" + getSlotValue(intent_message.slots, "tempChangeType", "1_00")
+#  temp.changeTemp(ObjectLocation, tempChangeType)
+#  hermes.publish_end_session(intent_message.session_id, "Temeperatur im {} wird verändert.".format(ObjectLocation))
 
 @catchErrors
 def setTemperature(hermes, intent_message):
@@ -111,6 +105,12 @@ def setTemperature(hermes, intent_message):
     hermes.publish_end_session(intent_message.session_id, "Temperatur im {} wird gesenkt.".format(ObjectLocation))
   else:
     hermes.publish_end_session(intent_message.session_id, "Was willst du von mir?")
+
+@catchErrors
+def getTemperature(hermes, intent_message):
+  ObjectLocation = getSlotValue(intent_message.slots, "ObjectLocation", intent_message.site_id if intent_message.site_id != "default" else "wohnzimmer")
+  tempType = getSlotValue(intent_message.slots, "tempType", "Ist")
+  hermes.publish_end_session(intent_message.session_id, "Die Temperatur im {} beträgt {} Grad.".format(ObjectLocation, temp.getStatus(ObjectLocation, tempType) / 100.0))
 
 @catchErrors
 def setRollerBlinds(hermes, intent_message):
@@ -142,17 +142,26 @@ def getRollerBlinds(hermes, intent_message):
     hermes.publish_end_session(intent_message.session_id, "Rollo im {} ist weit geschlossen.".format(location))
   elif tmp == 100:
     hermes.publish_end_session(intent_message.session_id, "Rollo im {} ist komplett geschlossen.".format(location))
-  
+
+@catchErrors
+def setObject(hermes, intent_message):
+  hermes.publish_end_session(intent_message.session_id, "Setzte Object!")
+
+@catchErrors
+def getObject(hermes, intent_message):
+  hermes.publish_end_session(intent_message.session_id, "Lade Object!")
 
 if __name__ == "__main__":
       configureLogger()
       h = assist.get_hermes()
       h.connect()
-      h.subscribe_intent("mdl:ActivateObjectCopy", activateObject)
-      h.subscribe_intent("mdl:DeactivateObjectCopy", deactivateObject)
-      h.subscribe_intent("mdl:GetObjectStatus", getObjectStatus)
+      #h.subscribe_intent("mdl:ActivateObjectCopy", activateObject)
+      #h.subscribe_intent("mdl:DeactivateObjectCopy", deactivateObject)
+      #h.subscribe_intent("mdl:GetObjectStatus", getObjectStatus)
       h.subscribe_intent("mdl:GetTemperature", getTemperature)
+      h.subscribe_intent("mdl:SetTemperature", setTemperature)
       h.subscribe_intent("mdl:SetRollerBlinds", setRollerBlinds)
       h.subscribe_intent("mdl:GetRollerBlinds", getRollerBlinds)
-      h.subscribe_intent("mdl:SetTemperature", setTemperature)
+      h.subscribe_intent("mdl:SetObject", setObject)
+      h.subscribe_intent("mdl:GetObject", getObject)
       h.loop_forever()
