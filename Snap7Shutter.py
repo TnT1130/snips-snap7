40175dd84eb3db1735c113f9b67f35178edae72c
TnT1130 <58545433+TnT1130@users.noreply.github.com>
2019-12-07 08:10:56 +0100

    Update Snap7Shutter.py

--- Snap7Shutter.py
+++ Snap7Shutter.py
@@ -5,24 +5,24 @@ class Snap7Shutter():
   def __init__(self, config):
     self.__client = s7con(config)
     self.__rooms = config.get("secret")["rooms"].lower().split(",")
-    self.__lightPos = "alle,fenster,tuer".lower().split(",")
+    self.__shutterPos = "fenster,tür".lower().split(",")
     self.__readDB = int(config.get("global")["shutterread"])
     self.__writeDB = int(config.get("global")["shutterwrite"])
     self.__readOffset = int(config.get("global")["ShutterReadOffset".lower()]) * 8   
     self.__writeOffset = int(config.get("global")["ShutterWriteOffset".lower()]) * 8 
-    if    self.__readOffset * 8 < len(self.__lightPos) * 8 * 2 \
-       or self.__writeOffset * 8 < len(self.__lightPos) * 8 * 4: 
+    if    self.__readOffset * 8 < len(self.__shutterPos) * 8 * 2 \
+       or self.__writeOffset * 8 < len(self.__shutterPos) * 8 * 4: 
        raise ValueError("Offset must be bigger than number shutterpositions")
 
   def __getReadOffset(self, room, pos):
     offset = self.__rooms.index(room.lower()) * self.__readOffset
-    offset += self.__lightPos.index(pos.lower()) * 8
+    offset += self.__shutterPos.index(pos.lower()) * 8
     lg.debug("room: {}, type: {}, ReadOffset: {}".format(room, pos, offset))
     return offset
 
   def __getWriteOffset(self, room, pos):
     offset = self.__rooms.index(room.lower()) * self.__writeOffset
-    offset += self.__lightPos.index(pos.lower()) * 8 * 2
+    offset += self.__shutterPos.index(pos.lower()) * 8 * 4
     lg.debug("room: {}, type: {}, WriteOffset: {}".format(room, pos, offset))
     return offset
 
