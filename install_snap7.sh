0b5c63337faf0636dd9d7d9586e01c1a4543a9ac
TnT1130 <58545433+TnT1130@users.noreply.github.com>
2019-12-30 11:29:47 +0100

    Update install_snap7.sh

--- install_snap7.sh
+++ install_snap7.sh
@@ -24,25 +24,25 @@ if [[ ! -s /usr/lib/libsnap7.so ]]; then
       exit 1
     fi
   fi
-  #wget https://netcologne.dl.sourceforge.net/project/snap7/1.4.2/$version.7z
-  #p7zip -d $version.7z
+  wget https://netcologne.dl.sourceforge.net/project/snap7/1.4.2/$version.7z
+  p7zip -d $version.7z
   #7z x $version.7z
   #build libsnpa7
-  #cd $version/build/unix
-  #make -f "$arch"_linux.mk
+  cd $version/build/unix
+  make -f "$arch"_linux.mk
   #install
   cp ../bin/$arch-linux/libsnap7.so /usr/lib
   ldconfig /usr/lib/libsnap7.so
   #cleanup
   cd ../../../
-  rm -r $version/
+  #rm -r $version/
   rm $version.7z
-  if [[ $p7zipExists == "" ]]; then
-    if [[ $(command -v apt) != "" ]]; then
-      apt autoremove -y p7zip-full
-    elif [[  $(command -v pacman) != "" ]]; then
-      pacman --noconfirm -Rs p7zip
-    fi
-  fi
+  #if [[ $p7zipExists == "" ]]; then
+  #  if [[ $(command -v apt) != "" ]]; then
+  #    apt autoremove -y p7zip-full
+  #  elif [[  $(command -v pacman) != "" ]]; then
+  #    pacman --noconfirm -Rs p7zip
+  #  fi
+  #fi
 fi
 exit 0
